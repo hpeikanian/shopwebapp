bc51cca42fdf54c6edd330b96d0cec685d6254e8
hpeikanian <124878930+hpeikanian@users.noreply.github.com>
2026-09-22 13:08:54 +0330

    feat: add editable navigation configuration

---
+++ src/config/navigation.ts
@@ -0,0 +1,9 @@
+export const navigation = [
+  { label: 'Home', href: '/' },
+  { label: 'Shop', href: '/shop' },
+  { label: 'Search', href: '/search' },
+  { label: 'Wishlist', href: '/wishlist' },
+  { label: 'Account', href: '/account' },
+];
+
+export const categories = ['Women', 'Men', 'Kids'];
