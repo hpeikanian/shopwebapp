1a5f794892e0bc038638da5ae8144a91ebfbe456
hpeikanian <124878930+hpeikanian@users.noreply.github.com>
2026-09-22 15:06:28 +0330

    fix: use default product card export

--- src/components/home/NewArrivals.tsx
+++ src/components/home/NewArrivals.tsx
@@ -1,5 +1,5 @@
 import { products } from '../../data/products';
-import { ProductCard } from '../product/ProductCard';
+import ProductCard from '../product/ProductCard';
 
 export function NewArrivals() {
   return (
