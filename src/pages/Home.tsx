36276a876045038edbeb64fb3cc7fd98b59d489d
hpeikanian <124878930+hpeikanian@users.noreply.github.com>
2026-09-22 13:12:58 +0330

    feat: assemble poodin home page sections

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,15 @@
-import HomeSections from '../components/home/HomeSections';
+import { HeroSection } from '../components/home/HeroSection';
+import { CategoryCards } from '../components/home/CategoryCards';
+import { NewArrivals } from '../components/home/NewArrivals';
+import { EditorialSection } from '../components/home/EditorialSection';
 
-export default function Home(){
-  return <main><HomeSections /></main>;
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-background text-foreground">
+      <HeroSection />
+      <CategoryCards />
+      <NewArrivals />
+      <EditorialSection />
+    </main>
+  );
 }
